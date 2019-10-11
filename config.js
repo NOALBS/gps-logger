193f892c1f66e82426f73f5973451074e360a6d8
b3ck <1740542+b3ck@users.noreply.github.com>
2019-10-11 16:54:42 -0700

    New Configuration file with comments.

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    PORT: 3000, //PORT to access webpage http://localhost:3000
+    RATELIMIT: 10000, //API Rate limit in milliseconds, 5000 = 5 seconds.
+    UNITS: 'imperial', // leave blank for standard, 'metric' for metric, 'imperial' for imperial.
+    OWM_Key: '<YOUR-API-KEY-HERE>', // API Key from http://OpenWeatherMap.org
+    LIQ_Key: '<YOUR-API-KEY-HERE>' // API Key from http://LocationIQ.com
+}
