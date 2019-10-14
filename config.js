2a687d503c5aafc2fb042ac376839e667d71432f
b3ck <1740542+b3ck@users.noreply.github.com>
2019-10-13 21:39:24 -0500

    Removed OLD APIs, added new API.

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 module.exports = {
     PORT: 3000, //PORT to access webpage http://localhost:3000
     RATELIMIT: 10000, //API Rate limit in milliseconds, 5000 = 5 seconds.
-    UNITS: 'imperial', // leave blank for standard, 'metric' for metric, 'imperial' for imperial.
-    OWM_Key: '<YOUR-API-KEY-HERE>', // API Key from http://OpenWeatherMap.org
-    LIQ_Key: '<YOUR-API-KEY-HERE>' // API Key from http://LocationIQ.com
+    UNITS: 'imperial',
+    OWM_Key: '<YOUR-APIKEY-HERE>', // API Key from http://OpenWeatherMap.org
+    HERE_appid: '<YOUR-APPID-HERE>', // Get AppID from https://developer.here.com/sign-up
+    HERE_appcode: '<YOUR-APPCODE-HERE>' // Get AppCode from https://developer.here.com/sign-up
 }
